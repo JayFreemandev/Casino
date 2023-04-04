8114839f2ab944f060a9cc97bea9c3e364184769
JayFreemandev <jayfreemandev@gmail.com>
2023-04-05 01:01:04 +0900

    Create BankStatementParser.java

---
+++ Transaction/BankStatementParser.java
@@ -0,0 +1,8 @@
+package com.example.real;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
