26cfd0851bc380db2ee282e8a1a33e180f5cd0e9
jay <jayfreemandev@gmail.com>
2023-05-03 23:16:47 +0900

    <test> transaction parsing method pass

--- src/test/java/transaction/BankStatementCSVParserTest.java
+++ src/test/java/transaction/BankStatementCSVParserTest.java
@@ -13,12 +13,12 @@ public class BankStatementCSVParserTest {
 
     @Test
     public void parseOneCorrectLine() throws Exception{
-        final String line = "2023-05-03, -22000, 마라탕";
+        final String line = "03-05-2023,-22000,마라탕";
         final BankTransaction result = bankStatementParser.parseFrom(line);
         final BankTransaction expected = new BankTransaction(LocalDate.of(2023, Month.MAY, 03), -22000, "마라탕");
         final double tolerance = 0.0d;
         Assert.assertEquals(expected.getDate(),result.getDate());
-        Assert.assertEquals(expected.getAmount(),result.getAmount());
+        Assert.assertEquals(expected.getAmount(),result.getAmount(), tolerance);
         Assert.assertEquals(expected.getDescription(),result.getDescription());
     }
 }
