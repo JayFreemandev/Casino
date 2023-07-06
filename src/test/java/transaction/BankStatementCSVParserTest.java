c0f5b763a1dfc1dad18b3149254b7190ba45a644
jay <jayfreemandev@gmail.com>
2023-07-06 16:02:45 +0900

    <feat> 테스트 커버리지 도구 Jacoco 추가

--- src/test/java/transaction/BankStatementCSVParserTest.java
+++ src/test/java/transaction/BankStatementCSVParserTest.java
@@ -1,26 +1,26 @@
 package transaction;
 
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
 import java.time.LocalDate;
 import java.time.Month;
+
+import static org.junit.Assert.*;
 
 public class BankStatementCSVParserTest {
     BankStatementParser parser = new BankStatementCSVParser();
 
     @Test
-    public void parseOneCorrectLine() throws Exception {
+    public void parseOneCorrectLine(){
         // given
-        final String line = "03-05-2023,-22000,마라탕";
+        final String line = "03-05-2023,-22000,콤푸 식권";
 
         // when
         BankTransaction bankTransaction = parser.parseFrom(line);
 
         // then
-        BankTransaction expected = new BankTransaction(LocalDate.of(2017, Month.JANUARY, 30), -50d, "콤푸 식권");
-        assertThat(bankTransaction.getDate()).isEqualTo(expected.getDate());
-        assertThat(bankTransaction.getAmount()).isEqualTo(expected.getAmount());
-        assertThat(bankTransaction.getDescription()).isEqualTo(expected.getDescription());
+        BankTransaction expected = new BankTransaction(LocalDate.of(2023, Month.MAY, 03), -22000, "콤푸 식권");
+        assertEquals(expected.getDate(), bankTransaction.getDate());
+        assertEquals(expected.getAmount(), bankTransaction.getAmount(), 0.001);
+        assertEquals(expected.getDescription(), bankTransaction.getDescription());
     }
 }
