ebfd6ed1609508f517077eb04da98e28c397b9f3
jay <jayfreemandev@gmail.com>
2023-07-17 17:03:54 +0900

    <refactor> Exception 기본 생성자 호출시 예외

--- src/main/java/agent/exception/AgentException.java
+++ src/main/java/agent/exception/AgentException.java
@@ -1,14 +1,16 @@
 package agent.exception;
 
+import static agent.global.AgentConstant.EXCEPTION_CLASS_CREATE_EXCEPTION_MESSAGE;
+
 public class AgentException extends RuntimeException {
     private final AgentExceptionType exceptionType;
+
+    private AgentException(){
+        throw new IllegalStateException(EXCEPTION_CLASS_CREATE_EXCEPTION_MESSAGE);
+    }
 
     public AgentException(AgentExceptionType exceptionType) {
         super(exceptionType.getMessage());
         this.exceptionType = exceptionType;
-    }
-
-    public AgentExceptionType getExceptionType() {
-        return exceptionType;
     }
 }
--- src/main/java/agent/global/AgentConstant.java
+++ src/main/java/agent/global/AgentConstant.java
@@ -3,6 +3,8 @@ package agent.global;
 public class AgentConstant {
     private static final String UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 Utility 클래스입니다.";
 
+    public static final String EXCEPTION_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 Exception 클래스입니다.";
+
     private AgentConstant() {
         throw new IllegalStateException(UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE);
     }
